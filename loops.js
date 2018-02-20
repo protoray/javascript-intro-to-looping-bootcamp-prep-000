ff8905d01cdbc6b367fe28545a1b543db9f527aa
Ray <ray@protozo.net>
2018-02-20 20:59:36 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,6 @@
 function forLoop(array)
 {
+<<<<<<< HEAD
   for(var i = 0; i < 25; i++)
   {
     if(i === 1)
@@ -34,4 +35,18 @@ function doWhileLoop(array)
     array.pop();
   } while (array.length > 2 && maybeTrue());
   return array;
+=======
+  for(let i = 0; i < 25; i++)
+  {
+    if(i === 1)
+    {
+      return `I am ${i} strange loop.`;
+    }
+    else
+    {
+      return `I am ${i+1} strange loops.`;
+    }
+    
+  }
+>>>>>>> 141f0fcb0713819eab73dbc1621001896bf80501
 }
